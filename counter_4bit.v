a441627523b41476530feda694f667b5bc7dd1b1
Jok <jacques-olivier.klein@u-psud.fr>
2025-03-25 13:59:53 +0100

    Create counter_4bit.v
    Add 4 bit counter

---
+++ counter_4bit.v
@@ -0,0 +1,32 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity counter_4bit is
+    Port (
+        clk   : in  STD_LOGIC;  -- Horloge
+        reset : in  STD_LOGIC;  -- Réinitialisation asynchrone
+        enable: in  STD_LOGIC;  -- Activation du comptage
+        load  : in  STD_LOGIC;  -- Chargement d'une valeur
+        data_in : in  STD_LOGIC_VECTOR(3 downto 0); -- Valeur à charger
+        count : out STD_LOGIC_VECTOR(3 downto 0)  -- Valeur du compteur
+    );
+end counter_4bit;
+
+architecture Behavioral of counter_4bit is
+    signal counter : STD_LOGIC_VECTOR(3 downto 0) := (others => '0');
+begin
+    process
+    begin
+    	wait until rising_edge(clk);
+        if reset = '1' then
+            counter <= (others => '0');  
+         elsif load = '1' then
+                counter <= data_in;  
+         elsif enable = '1' then
+                counter <= counter + 1; 
+          end if;
+    end process;
+    count <= counter;
+end Behavioral;
